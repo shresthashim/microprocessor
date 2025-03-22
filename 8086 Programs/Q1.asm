db8b2651f28874da36def70d5d781690c62af40a
AsHim Shrestha <ashimsth89@gmail.com>
2025-03-22 09:46:44 +0545

    Refactor 8086 program to print "HELLO" and "WORLD" on separate lines with proper formatting

--- 8086 Programs/Q1.asm
+++ 8086 Programs/Q1.asm
@@ -1,17 +1,37 @@
-.model small
-.stack 100h
+.model small           ; Use the small memory model (for programs < 64KB)
+.stack 100h           ; Allocate 256 bytes for the stack
 .data
-    A db "HELLO WORLD$"   ; String must end with '$' for DOS interrupt 09h
+    msg1 db "HELLO$"  ; Define first string, '$' marks the end for DOS interrupt 09h
+    msg2 db "WORLD$"  ; Define second string
+
 .code
 main proc far
-    mov ax, @data
-    mov ds, ax
-    
-    mov dx, offset A
-    mov ah, 09h
-    int 21h
+    ; Set up the Data Segment
+    mov ax, @data    ; Load the data segment address into AX
+    mov ds, ax       ; Copy AX into DS to set the data segment
 
-    mov ah, 4Ch
-    int 21h
+    ; Print "HELLO" using DOS interrupt 09h
+    lea dx, msg1     ; Load the address of msg1 into DX
+    mov ah, 09h      ; DOS function 09h - Print string
+    int 21h          ; Call DOS interrupt
+
+    ; Print a New Line (CR + LF) for proper formatting
+    mov dl, 13       ; Carriage Return (CR) - Moves cursor to the start of the line
+    mov ah, 2        ; DOS function 02h - Print character
+    int 21h          ; Call DOS interrupt
+
+    mov dl, 10       ; Line Feed (LF) - Moves cursor down to the next line
+    mov ah, 2        ; DOS function 02h - Print character
+    int 21h          ; Call DOS interrupt
+
+    ; Print "WORLD" using DOS interrupt 09h
+    lea dx, msg2     ; Load the address of msg2 into DX
+    mov ah, 09h      ; DOS function 09h - Print string
+    int 21h          ; Call DOS interrupt
+
+    ; Terminate the program properly
+    mov ah, 4Ch      ; DOS function 4Ch - Exit program
+    int 21h          ; Call DOS interrupt
+
 main endp
-end main
+end main             ; End of program, execution starts from "main"
