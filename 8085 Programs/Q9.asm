e2fc2e54506a36d9136aedfc7790af031bd59c2c
AsHim Shrestha <ashimsth89@gmail.com>
2025-04-16 10:05:50 +0545

    Refactor Q9.asm to improve code clarity and structure for counting even and odd parity numbers

--- 8085 Programs/Q9.asm
+++ 8085 Programs/Q9.asm
@@ -1,23 +1,26 @@
 ;Count the even and odd parity numbers of 150 data stored in memory starting from C050H. Store the counts in D000H and D001H respectively.
 
-LXI H, C050H ; Load HL with the starting address of the data
-MVI C, 96H
-MVI D, 00H ; COunt of odd parity
-MVI E, 00H ; Count of even parity
+LXI H, C050H    ; HL points to start of data
+MVI C, 96H      ; 150 bytes = 96H (in hex)
+MVI D, 00H      ; D = odd parity count
+MVI E, 00H      ; E = even parity count
 
-MOV A,M 
-ADI 00H 
-JPE SKIP     
-INR D 
-JMP DOWN  
-BEL : INR E
+LOOP: MOV A, M      ; Get data from memory
+      ADI 00H       ; Refresh flags
+      JPE EVEN      ; If even parity, jump to EVEN
+      INR D         ; Else, increment odd parity count
+      JMP NEXT
 
-SKIP : INX H
-DCR C
-JNZ SKIP  
+EVEN: INR E         ; Increment even parity count
+
+NEXT: INX H         ; Move to next byte
+      DCR C         ; Decrease counter
+      JNZ LOOP      ; Repeat if C ≠ 0
+
 MOV A, D
-STA D000H ; Store the count of odd parity numbers at D000H
+STA D000H           ; Store odd parity count
+
 MOV A, E
-STA D001H ; Store the count of even parity numbers at D001H
+STA D001H           ; Store even parity count
+
 HLT
-END
